1417a2de984726c770fc5d1b5968b297fe47ad39
Gosia <mkrzek@googlemail.com>
2018-07-19 15:53:30 +0100

    added cookie-session

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 //nothing is returned from the below statement so we can just condense it
-const passportConfig = require("./services/passport");
-//condensed version of what is above
+require("./models/User.js");
 require("./services/passport");
-require("./models/user.js");
+
+//condensed version of what is above
+
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
 );
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 //app server; get =registers route handler; req incoming request; res=response data sent to whomever made a resquest
