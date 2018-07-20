23d646741cdabfd254c0dcbd8326ed9385176884
Gosia <mkrzek@googlemail.com>
2018-07-20 13:19:10 +0100

    added files

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js-figure out what set of credentials to return
 //tjis is set up by heroku
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
+  module.exports = require("./prod.js");
 } else {
-  module.exports = require("./dev");
+  module.exports = require("./dev.js");
 }
