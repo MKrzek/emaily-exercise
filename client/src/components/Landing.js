be5893c4d70d1d04db0f503a0ecdce446e5e56fe
Malgorzata <mkrzek@googlemail.com>
2018-08-27 19:01:26 +0100

    updating scripts

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Landing = () => {
+    console.log('landing loaded up')
     return (<div style={{ textAlign: 'center' }}><h1>Emaily!</h1>
         Collect feedback from your users
     </div>
