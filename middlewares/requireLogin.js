9e8e1f86bbf16143e5af8d9f2331c3a5bd6d1249
Gosia <mkrzek@googlemail.com>
2018-08-28 10:17:17 +0100

    corrected errors

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -4,4 +4,5 @@ module.exports = (req, res, next) => {
   if (!req.user) {
     return res.status(403).send({ error: "You must log in!" });
   }
+  next();
 };
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,13 +3,16 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", requireLogin, async (req, res) => {
+  app.post("/api/stripe", async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     });
+    if (!charge.paid) {
+      return res.status(400).send({ error: "have not paid" });
+    }
     console.log("charge", charge);
     req.user.credits += 5;
     //take the model and persist it to the database
