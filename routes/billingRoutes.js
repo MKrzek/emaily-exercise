862a93c5520293fcdc0d39f85dc6509e0172b06b
Gosia <mkrzek@googlemail.com>
2018-08-28 10:34:28 +0100

    added login

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,17 +3,13 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     });
-    if (!charge.paid) {
-      return res.status(400).send({ error: "have not paid" });
-    }
-    console.log("charge", charge);
     req.user.credits += 5;
     //take the model and persist it to the database
     const user = await req.user.save();
