d50ec5672a578bfe2dbfb08c4b372d0d30eb51a5
Gosia <mkrzek@googlemail.com>
2018-09-11 11:08:06 +0100

    added email validate function

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,13 +1,14 @@
 //SurveyField contains label and text input 
 import React from 'react';
-export default ({input, label})=>{
+export default ({input, label, meta: {error, touched}})=>{
     //props.input
     //{...input} equals to onBlur={input.onBlur}
     console.log('props', input )
     return ( 
         <div>
              <label>{label}</label>
-            <input {...input}/>
+            <input {...input} style={{marginBottom: "5px"}}/>
+            <div className='red-text' style={{marginBottom: '20px'}}>{touched  && error}</div>
         </div>
     )
 }
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import _ from 'lodash'
 import { reduxForm, Field } from 'redux-form'
+import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails'
+
 const FIELDS=[
     {name:'title', label:'Survey Title'},
     {name:'subject', label:'Subject Line'},
-    {label:'Email Body', name:'body' },
-    {label:'Recipient List', name:'emails'}];
+    { name:'body', label:'Email Body' },
+    {name:'emails', label:'Recipient List'}];
+
 class SurveyForm extends React.Component {
 
 
@@ -22,11 +26,38 @@ class SurveyForm extends React.Component {
         return <div>
             <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                {this.renderFields()}
-                <button type='submit'>Submit</button>
+               <Link to='/surveys' className='red btn-flat left text-white'>Cancel</Link>
+                <button   className='teal btn-flat right white-text' type='submit'>Next
+                <i className='material-icons right'>done</i>
+                </button>
             </form>
         </div>
     }
 }
+function validate (values){
+    const errors={}
+    errors.emails= validateEmails(values.emails||'')
+    
+    _.each(FIELDS, ({name, noError})=>{
+        console.log('values[name]', values[name])
+        if(!values[name]){
+            errors[name]='You must provide a value'
+        }
+        
+
+   })
+    // if (!values.title){
+    //     errors.title="You must provide a title"
+    // }
+    // if (!values.subject){
+    //     errors.subject="You must provide a subject"
+    // }
+    // if(!values.body){
+    //     errors.body="You must provide a content"
+    // }
+    return errors
+}
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm)
